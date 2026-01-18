f4940d2cabaebabfd1b0a51a88e80e3528b0309b
Peng <penggeng@g.ucla.edu>
2026-01-17 23:24:26 -0800

    Update structure_factor_cal.py

--- viz_analysis/structure_factor_cal.py
+++ viz_analysis/structure_factor_cal.py
@@ -1,3 +1,32 @@
+#!/usr/bin/python3
+#########################################################################
+#    SPF - Stochastic Phase Field
+#    Copyright (C) 2025 
+#    Peng Geng <penggeng@g.ucla.edu>
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+#    See the README file in the top-level SPF directory.
+#########################################################################
+# File: structure_factor_cal.py
+# Purpose: This script computes the structure factor from the simulation
+# output HDF5 file using the method described in Section 2.2 of the
+# referenced paper.
+# Requirements: numpy, h5py, matplotlib, scipy, openpyxl
+
+
 import h5py
 import numpy as np
 
